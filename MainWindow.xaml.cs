cbffb7352273fb5a06094df738b4e0185fedc490
vorvit <94832434+vorvit@users.noreply.github.com>
2021-12-26 00:38:04 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF15
+{
+    /*ЗАДАНИЕ 15. ДОКУМЕНТЫ И ПЕЧАТЬ
+        Подготовить фиксированный документ по произвольной теме. Проявите вкус и фантазию при оформлении.
+        Обязательно наличие блочных элементов Paragraph, List, Table.
+     */
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
